eac242519fcf1ae1af6c8e533bb9b9bc9db66160
Eo-x <1619382576@qq.com>
2020-04-30 21:59:03 +0800

    Auth 模块负责登录认证  后台管理权限配置等

---
+++ Auth/src/main/java/com/sy/auth/AuthApplication.java
@@ -0,0 +1,18 @@
+package com.sy.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户认证授权，配置等服务类
+ * @author wangxiao
+ * @date 2020-04-30
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
