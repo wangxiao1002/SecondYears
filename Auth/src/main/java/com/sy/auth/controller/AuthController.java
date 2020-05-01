f8ed159f55baa4107ce8f1cf5e895f6f819815ba
wangxiao1002 <Eo_xiao@163.com>
2020-05-01 23:37:17 +0800

    用户简易登录后台

--- Auth/src/main/java/com/sy/auth/controller/AuthController.java
+++ Auth/src/main/java/com/sy/auth/controller/AuthController.java
@@ -5,12 +5,14 @@ import com.baomidou.kaptcha.exception.KaptchaIncorrectException;
 import com.baomidou.kaptcha.exception.KaptchaNotFoundException;
 import com.baomidou.kaptcha.exception.KaptchaRenderException;
 import com.baomidou.kaptcha.exception.KaptchaTimeoutException;
+import com.sy.auth.facde.service.AuthService;
 import com.sy.basis.common.BaseResult;
 import com.sy.basis.common.ResultStatus;
 import com.sy.basis.domain.LoginDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
 
 /**
@@ -22,8 +24,11 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
 
-
     private  Kaptcha kaptcha;
+
+
+    @Autowired
+    private AuthService authService;
 
     @Autowired
     public void setKaptcha(Kaptcha kaptcha) {
@@ -31,23 +36,31 @@ public class AuthController {
     }
 
     @PostMapping("/authLogin")
-    public BaseResult authLogin(@RequestBody LoginDO loginDO) {
-        return null;
+    public BaseResult<Object> authLogin(@RequestBody LoginDO loginDO) {
+        BaseResult<Object> response = new BaseResult<>();
+        if (!validWithTime(loginDO.getVerifyCode())) {
+            return new BaseResult<>().status(ResultStatus.FAIL).message("验证码过期");
+        }
+
+        LoginDO tempLoginDO = authService.queryUserByInfo(loginDO);
+        Optional.ofNullable(tempLoginDO)
+                .map(temp -> response.result(authService.queryMenus(temp.getCode())))
+                .orElse(response.status(ResultStatus.FAIL).message("用户密码不正确"));
+        return response;
     }
 
-    @GetMapping(value = "/get")
+    @GetMapping(value = "/verifyCode")
     public void render() {
         kaptcha.render();
     }
 
 
-    public String validWithTime(String code) {
-
+    public boolean validWithTime(String code) {
         try {
             kaptcha.validate(code,60);
-            return ResultStatus.SUCCESS;
+            return true;
         }catch (KaptchaIncorrectException | KaptchaNotFoundException | KaptchaRenderException | KaptchaTimeoutException exception){
-            return ResultStatus.FAIL;
+            return false;
         }
     }
 
---
+++ Basis/src/main/java/com/sy/basis/domain/MenuDTO.java
@@ -0,0 +1,54 @@
+package com.sy.basis.domain;
+
+import com.sy.basis.common.BaseDO;
+
+import java.util.List;
+
+/**
+ * 菜单
+ * @author wangxiao
+ * @since 1.1
+ */
+public class MenuDTO extends BaseDO {
+
+    private String label;
+
+    private String code;
+
+    private Integer order;
+
+
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuDTO{" +
+                "label='" + label + '\'' +
+                ", code='" + code + '\'' +
+                ", order=" + order +
+                '}';
+    }
+}
