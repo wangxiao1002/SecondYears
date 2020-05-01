7af9ff3513adc4b808653462e672e16c827db350
wangxiao1002 <Eo_xiao@163.com>
2020-05-01 22:47:36 +0800

    使用docker 启动

--- Auth/src/main/java/com/sy/auth/AuthApplication.java
+++ Auth/src/main/java/com/sy/auth/AuthApplication.java
@@ -2,12 +2,14 @@ package com.sy.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 用户认证授权，配置等服务类
  * @author wangxiao
  * @date 2020-04-30
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthApplication {
 
--- Auth/src/main/java/com/sy/auth/controller/AuthController.java
+++ Auth/src/main/java/com/sy/auth/controller/AuthController.java
@@ -9,12 +9,9 @@ import com.sy.basis.common.BaseResult;
 import com.sy.basis.common.ResultStatus;
 import com.sy.basis.domain.LoginDO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+
 
 /**
  * 登录
