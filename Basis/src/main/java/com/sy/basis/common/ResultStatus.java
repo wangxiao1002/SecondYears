98c76b3626026b341386f709578dcaebe155b90c
Eo-x <1619382576@qq.com>
2020-05-01 20:33:51 +0800

    Kaptcha 验证码 使用

---
+++ Basis/src/main/java/com/sy/basis/common/ResultStatus.java
@@ -0,0 +1,11 @@
+package com.sy.basis.common;
+
+/**
+ * 结果状态
+ * @author wangxiao
+ * @since
+ */
+public class ResultStatus {
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+}
