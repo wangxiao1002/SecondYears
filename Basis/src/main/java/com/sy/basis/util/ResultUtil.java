e176e731b640039a1db706bc3d5e43806978a0a7
wangxiao1002 <Eo_xiao@163.com>
2020-05-03 12:08:41 +0800

    权限认证、 crud

---
+++ Basis/src/main/java/com/sy/basis/util/ResultUtil.java
@@ -0,0 +1,56 @@
+package com.sy.basis.util;
+
+import com.sy.basis.common.BaseResult;
+import com.sy.basis.common.ResultStatus;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 
+ * @author wangxiao
+ * @since
+ */
+public class ResultUtil {
+
+    private ResultUtil() {
+    }
+
+
+    private final static String EMPTY_MESSAGE = "数据内容为空";
+
+    private final static List EMPTY_LIST =  new ArrayList(0);
+
+    public static BaseResult<String> success(String message) {
+        return new BaseResult<>(ResultStatus.SUCCESS, message, null);
+    }
+
+    public static <T> BaseResult<T> success(T result) {
+        return new BaseResult<>(ResultStatus.SUCCESS, null, result);
+    }
+
+    public static <T> BaseResult<T> success(String message, T result) {
+        return new BaseResult<>(ResultStatus.SUCCESS, message, result);
+    }
+
+    public static BaseResult<? extends Collection> empty() {
+        return new BaseResult<>(ResultStatus.SUCCESS, EMPTY_MESSAGE, EMPTY_LIST);
+    }
+
+    public static BaseResult<String> isnull(String message) {
+        return new BaseResult<>(ResultStatus.SUCCESS, message, null);
+    }
+
+    public static BaseResult<String> fail(String message) {
+        return new BaseResult<>(ResultStatus.FAIL, message, null);
+    }
+
+    public static <T> BaseResult<T> fail(String message, T result) {
+        return new BaseResult<>(ResultStatus.FAIL, message, result);
+    }
+
+    public static BaseResult fail(Throwable e) {
+        return fail(e.getMessage());
+    }
+}
