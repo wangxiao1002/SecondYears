63931879aef9940c2b1133c27699131dd91b0ca2
wangxiao1002 <Eo_xiao@163.com>
2020-05-15 22:06:46 +0800

    SYIM 项目构建

---
+++ SYIM/SYIM-Server/src/main/java/com/sy/syimserver/SyImServerApplication.java
@@ -0,0 +1,13 @@
+package com.sy.syimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyImServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyImServerApplication.class, args);
+    }
+
+}
---
+++ SYIM/SYIM-Server/src/test/java/com/sy/syimserver/SyImServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.syimserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyImServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
