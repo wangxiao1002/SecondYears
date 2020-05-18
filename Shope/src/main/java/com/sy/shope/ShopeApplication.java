cedbcac096cf627ee9ce4ca0d6c6afbae8171dbb
wangxiao1002 <Eo_xiao@163.com>
2020-05-18 21:07:03 +0800

    添加商城模块

---
+++ Shope/src/main/java/com/sy/shope/ShopeApplication.java
@@ -0,0 +1,13 @@
+package com.sy.shope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeApplication.class, args);
+    }
+
+}
---
+++ Shope/src/test/java/com/sy/shope/ShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.shope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
