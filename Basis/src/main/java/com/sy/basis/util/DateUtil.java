4cd1f4ae809de2ab42bb50a159eaaeb159ceedff
wangxiao1002 <Eo_xiao@163.com>
2020-05-20 11:02:47 +0800

    日期工具类

--- Basis/src/main/java/com/sy/basis/util/DateUtil.java
+++ Basis/src/main/java/com/sy/basis/util/DateUtil.java
@@ -1,6 +1,4 @@
-package com.sy.syimserver.util;
-
-import sun.util.resources.LocaleData;
+package com.sy.basis.util;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,23 +12,43 @@ import java.util.Locale;
  */
 public class DateUtil {
 
-    private DateUtil () {}
+    private DateUtil() {}
 
     private static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
     private static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.CHINA);
 
-
+    /**
+     *  格式化日期时间
+     * @param dateTime 日期时间
+     * @return String
+     */
     public static String formatDateTime (LocalDateTime dateTime) {
         return dateTime.format(DATE_TIME_FORMATTER);
     }
 
+    /**
+     *  转换日期时间
+     * @param dateTime 日期时间
+     * @return String
+     */
     public static LocalDateTime parseDateTime (String dateTime) {
         return LocalDateTime.parse(dateTime,DATE_TIME_FORMATTER);
     }
 
+    /**
+     *  格式化日期
+     * @param localDate 日期
+     * @return String
+     */
     public static String formatDate (LocalDate localDate) {
         return localDate.toString();
     }
+
+    /**
+     *  转换日期
+     * @param date 日期
+     * @return String
+     */
     public static LocalDate parseDate (String date) {
         return LocalDate.parse(date,DATE_FORMATTER);
     }
