cbd7e659f4caa54ee3609302445ba720a721f8dc
wangxiao1002 <Eo_xiao@163.com>
2020-05-21 21:54:36 +0800

    消息模块

---
+++ Notify-server/src/main/java/com/sy/notifyserver/NotifyServerApplication.java
@@ -0,0 +1,13 @@
+package com.sy.notifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyServerApplication.class, args);
+    }
+
+}
---
+++ Notify-server/src/test/java/com/sy/notifyserver/NotifyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.notifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
