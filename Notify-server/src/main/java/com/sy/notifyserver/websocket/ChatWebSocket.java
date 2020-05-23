a2b6b938175c5193466807db9073130961897647
wangxiao1002 <Eo_xiao@163.com>
2020-05-23 22:11:21 +0800

    聊天功能简易完成

--- Notify-server/src/main/java/com/sy/notifyserver/websocket/ChatWebSocket.java
+++ Notify-server/src/main/java/com/sy/notifyserver/websocket/ChatWebSocket.java
@@ -1,10 +1,14 @@
 package com.sy.notifyserver.websocket;
 
+import com.sy.notifyserver.util.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.util.List;
 
 /**
  * 聊天室webSocket
@@ -15,17 +19,40 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/chat/{chatId}")
 public class ChatWebSocket {
 
+    private Logger logger = LoggerFactory.getLogger(ChatWebSocket.class);
+
     @OnOpen
-    public void onOpen (Session session, @PathParam("chatId") String chatId) {}
+    public void onOpen (Session session, @PathParam("chatId") String chatId) {
+        WebSocketManager.addSession(chatId,session);
+        logger.info("聊天ID: {}有新的人加入,当前在线人数:{}",chatId,WebSocketManager.count(chatId));
+        sendMessage(chatId,SocketMessage.build().fromId("").message("欢迎加入"));
+    }
 
     @OnMessage
-    public void onMessage (String message) {}
+    public void onMessage (@PathParam("chatId") String chatId,String message) {
+        logger.info("聊天ID: {}接收到消息",chatId);
+        sendMessage(chatId,SocketMessage.build().fromId("").message(message));
+    }
 
     @OnClose
-    public void onClose(Session session) {}
+    public void onClose(@PathParam("chatId") String chatId,Session session) {
+        logger.info("聊天ID: {}关闭",chatId);
+    }
 
     @OnError
-    public void onError (Session session) {}
+    public void onError (@PathParam("chatId") String chatId,Session session,Throwable error) {
+        logger.info("聊天ID: {},聊天异常: {}",chatId,error.getMessage());
+    }
 
-    private void  sendMessage (String chatId,SocketMessage message) {}
+    private void  sendMessage (String chatId,SocketMessage message) {
+        List<Session>  sessionList = WebSocketManager.session(chatId);
+
+        if (null == sessionList || sessionList.isEmpty()) {
+            return;
+        }
+        // 暂时 不用线程池
+        for (Session session : sessionList) {
+            session.getAsyncRemote().sendText(JsonUtil.convertString(message));
+        }
+    }
 }
--- Notify-server/src/main/java/com/sy/notifyserver/websocket/WebSocketManager.java
+++ Notify-server/src/main/java/com/sy/notifyserver/websocket/WebSocketManager.java
@@ -2,7 +2,9 @@ package com.sy.notifyserver.websocket;
 
 import javax.websocket.Session;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * webSocket 管理类
@@ -32,7 +34,49 @@ public class WebSocketManager {
      * @param session value
      */
     public static void addSession(String chatId,Session session) {
-
+        LinkedList<Session> sessions = CHAT_SESSION.computeIfAbsent(chatId,key ->new LinkedList<>());
+        sessions.add(session);
+        CHAT_COUNT.compute(chatId,(key,value) -> {
+            if (null == value) {
+                value = 0;
+            }else {
+                value += 1;
+            }
+            return value;
+        });
     }
 
+    /**
+     * 移除session
+     * @param chatId
+     * @param session
+     */
+    public static void removeSession (String chatId,Session session) {
+        AtomicInteger sessionSize = new AtomicInteger();
+        CHAT_SESSION.computeIfPresent(chatId,(key,value) -> {
+            value.remove(session);
+            sessionSize.set(value.size());
+            return value;
+        });
+        if (sessionSize.get() == 0 ) {
+            CHAT_SESSION.remove(chatId);
+        }
+        AtomicInteger countSize = new AtomicInteger();
+        CHAT_COUNT.computeIfPresent(chatId,(key,value) -> {
+             value -= 1;
+             countSize.set(value);
+             return value;
+        });
+        if (countSize.get() == 0 ) {
+            CHAT_COUNT.remove(chatId);
+        }
+    }
+
+    public static int count (String chatId) {
+        return CHAT_COUNT.get(chatId);
+    }
+
+    public static List<Session> session (String chatId) {
+        return CHAT_SESSION.get(chatId);
+    }
 }
