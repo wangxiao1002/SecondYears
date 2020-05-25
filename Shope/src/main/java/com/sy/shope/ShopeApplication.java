fc8594c315069deca1c229020a67f47953cdd057
wangxiao1002 <Eo_xiao@163.com>
2020-05-25 23:15:55 +0800

    商城开始开发

--- Shope/src/main/java/com/sy/shope/ShopeApplication.java
+++ Shope/src/main/java/com/sy/shope/ShopeApplication.java
@@ -3,11 +3,16 @@ package com.sy.shope;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+ /**
+  * 启动类
+  * @author wangxiao
+  **/
 @SpringBootApplication
-public class ShopeApplication {
+public class ShopeApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopeApplication.class, args);
     }
 
 }
+
