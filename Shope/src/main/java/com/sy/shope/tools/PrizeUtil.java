7f2fc6f7282b631a4cc39aa6db707b617ca65b1c
wangxiao1002 <Eo_xiao@163.com>
2020-05-26 23:34:52 +0800

    抽奖

--- Shope/src/main/java/com/sy/shope/tools/PrizeUtil.java
+++ Shope/src/main/java/com/sy/shope/tools/PrizeUtil.java
@@ -2,6 +2,7 @@ package com.sy.shope.tools;
 
 import com.sy.shope.entity.PrizeDomain;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 /**
@@ -13,6 +14,35 @@ public class PrizeUtil {
 
 
     public String calcPrizeCode (List<PrizeDomain> prizeDomains) {
+        DecimalFormat format = new DecimalFormat("#####00.00");
+
+        String code ="";
+        double sumWeight = 0;
+        for (PrizeDomain prizeDomain : prizeDomains) {
+            sumWeight += prizeDomain.getProbability();
+        }
+        double random= Math.random();
+
+        double d1 = 0;
+        double d2 = 0;
+        for(int i=0;i<prizeDomains.size();i++){
+            d2 += Double.parseDouble(String.valueOf(prizeDomains.get(i).getProbability()))/sumWeight;
+            if(i==0){
+                d1 = 0;
+            }else{
+                d1 +=Double.parseDouble(String.valueOf(prizeDomains.get(i-1).getProbability()))/sumWeight;
+            }
+            if(random >= d1 && random <= d2){
+                code = prizeDomains.get(i).getCode();
+                break;
+            }
+        }
+
+        return code;
+    }
+
+
+    public static void main(String[] args) {
 
     }
 }
