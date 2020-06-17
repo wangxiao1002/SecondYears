e374b50e48b3114e8d6b8b3f84870eda913ab8fc
wangxiao1002 <Eo_xiao@163.com>
2020-06-17 23:32:29 +0800

    gateway 尝试

---
+++ gateway-gateway/src/main/java/com/sy/gatewaygateway/GatewayGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sy.gatewaygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayGatewayApplication.class, args);
+    }
+
+}
---
+++ gateway-gateway/src/test/java/com/sy/gatewaygateway/GatewayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.gatewaygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
