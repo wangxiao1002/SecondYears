97a0453174d279f7b644e199c52c4cba6a3393e7
wangxiao1002 <Eo_xiao@163.com>
2020-07-07 22:41:09 +0800

    结合eurake

--- Shope/src/main/java/com/sy/shope/ShopeApplication.java
+++ Shope/src/main/java/com/sy/shope/ShopeApplication.java
@@ -3,11 +3,14 @@ package com.sy.shope;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
- /**
+/**
   * 启动类
   * @author wangxiao
   **/
+
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan(value = {"com.sy.shope.mappers"})
 public class ShopeApplication  {
