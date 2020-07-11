885432e9d57b5acb5f57714daee165a7c5374fae
wangxiao1002 <Eo_xiao@163.com>
2020-07-11 23:17:28 +0800

    百度地图api 工具预留

---
+++ Basis/src/main/java/com/sy/basis/util/BaiduMapUtil.java
@@ -0,0 +1,14 @@
+package com.sy.basis.util;
+
+/**
+ * 百度地图api
+ * @author wangxiao
+ * @since 1.1
+ */
+public class BaiduMapUtil {
+
+    private final static String AK ="xQjYqVgtVeLa8sltPZvMkiIglpxWjS7p";
+
+
+
+}
