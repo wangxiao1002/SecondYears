1c254b547b8f0b97ecfa9472f28e3843bfaa4732
wangxiao1002 <Eo_xiao@163.com>
2020-07-13 23:41:06 +0800

    百度地图api 工具

--- Basis/src/main/java/com/sy/basis/util/BaiduMapUtil.java
+++ Basis/src/main/java/com/sy/basis/util/BaiduMapUtil.java
@@ -1,11 +1,12 @@
 package com.sy.basis.util;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import com.fasterxml.jackson.databind.JsonNode;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.time.Duration;
 
 /**
  * 百度地图api
@@ -15,46 +16,42 @@ import org.apache.http.util.EntityUtils;
 public class BaiduMapUtil {
 
 
-    private final static String LANDMARK_URL = "http://api.map.baidu.com/reverse_geocoding/v3/?ak=xQjYqVgtVeLa8sltPZvMkiIglpxWjS7p&output=json&coordtype=wgs84ll&location=LATI,LONG";
-
-    private final static HttpClient CLIENT = HttpClientBuilder.create().build();
+    private final static OkHttpClient CLIENT = new OkHttpClient.Builder()
+            .connectTimeout(Duration.ofSeconds(10))
+            .writeTimeout(Duration.ofSeconds(5))
+            .readTimeout(Duration.ofSeconds(5))
+            .callTimeout(Duration.ofSeconds(15))
+            .build();
     
 
     public static JsonNode  getLandmark (String latitude,String longitude) {
-        String url = LANDMARK_URL.replace("LATI",latitude).replace("LONG",longitude);
-        JsonNode result = null;
-        HttpGet get = new HttpGet(url);
-        try {
-            HttpResponse response = CLIENT.execute(get);
-            HttpEntity resp = response.getEntity();
-            String content = EntityUtils.toString(resp);
-            JsonNode  respResult = JsonUtil.parseJsonToJsonNode(content);
-            if (Constants.ERROR_CODE.equals(respResult.getString(Constants.STATUS).asText())) {
-                result = respResult.get("result");
-            }
-        }catch (Exception e) {
-            return result;
-        }
-        return result;
+        String location = String.format("%s,%s",latitude,longitude);
+        return getLandmark(location);
     }
 
-    public static JsonNode  getLandmark (String location) {
+    public static JsonNode getLandmark (String location) {
         JsonNode result = null;
         String url = "http://api.map.baidu.com/reverse_geocoding/v3/?ak=xQjYqVgtVeLa8sltPZvMkiIglpxWjS7p&output=json&coordtype=wgs84ll&location="+location;
-        try {
-            HttpGet get = new HttpGet(url);
-            HttpResponse response = CLIENT.execute(get);
-            HttpEntity resp = response.getEntity();
-            String content = EntityUtils.toString(resp);
-            JsonNode  respResult = JsonUtil.parseJsonToJsonNode(content);
-            if (Constants.ERROR_CODE.equals(respResult.getString(Constants.STATUS).asText())) {
-                result = respResult.get("result");
-            }
-        }catch (Exception e) {
-            return result;
-        }
-        return result;
+        return callBaiMap(url);
     }
+
+    private static JsonNode callBaiMap (String url) {
+        okhttp3.Request request = new okhttp3.Request.Builder().url(url).get().build();
+        final Call call = CLIENT.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                String respBody = response.body().string();
+                JsonNode jsonNode = JsonUtil.parseJsonToJsonNode(respBody);
+                return jsonNode.get("result");
+            }
+            return null;
+        }catch (Exception e) {
+            return null;
+        }
+    }
+
+
 
 
 }
--- Basis/src/main/java/com/sy/basis/util/Constants.java
+++ Basis/src/main/java/com/sy/basis/util/Constants.java
@@ -1,3 +1,5 @@
+package com.sy.basis.util;
+
 import java.time.format.DateTimeFormatter;
 
 /**
