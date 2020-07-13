f8d88067ce26406444381699e32e0d418e583eae
王潇 <Eo_xiao@163.com>
2020-07-13 13:56:30 +0800

    Create Constants.java

---
+++ Basis/src/main/java/com/sy/basis/util/Constants.java
@@ -0,0 +1,17 @@
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author: wang xiao
+ * @description:
+ * @date: Created in 15:40 2020/7/2
+ */
+public final class Constants {
+
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final String  ERROR_CODE = "0";
+
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+    public final static String STATUS ="status";
+
+}
