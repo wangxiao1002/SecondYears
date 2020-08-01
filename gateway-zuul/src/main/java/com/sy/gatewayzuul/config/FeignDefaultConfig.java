38bb2e8db2de0f3e1c295b506c00f8f011220177
wangxiao1002 <Eo_xiao@163.com>
2020-08-01 20:51:24 +0800

    feign 处理

---
+++ gateway-zuul/src/main/java/com/sy/gatewayzuul/config/FeignDefaultConfig.java
@@ -0,0 +1,52 @@
+package com.sy.gatewayzuul.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * feign config
+ * @author wangxiao
+ * @since 1.1
+ */
+public class FeignDefaultConfig implements RequestInterceptor {
+
+
+    private final Logger logger = LoggerFactory.getLogger(FeignDefaultConfig.class);
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                String values = request.getHeader(name);
+                requestTemplate.header(name, values);
+            }
+        }
+        Enumeration<String> bodyNames = request.getParameterNames();
+        StringBuffer body =new StringBuffer();
+        if (bodyNames != null) {
+            while (bodyNames.hasMoreElements()) {
+                String name = bodyNames.nextElement();
+                String values = request.getParameter(name);
+                body.append(name).append("=").append(values).append("&");
+            }
+        }
+        if(body.length()!=0) {
+            body.deleteCharAt(body.length()-1);
+            requestTemplate.body(body.toString());
+            logger.info("feign interceptor body:{}",body.toString());
+        }
+    }
+
+}
